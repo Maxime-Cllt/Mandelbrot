fce54ed19a4f86a0c03024a413023bfcd1d74137
Maxime <98154358+Maxime-Cllt@users.noreply.github.com>
2023-11-18 11:06:11 +0100

    Ajout de la class Mandelbrot.java

---
+++ server/Mandelbrot.java
@@ -0,0 +1,13 @@
+package server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Mandelbrot extends Remote {
+
+    void addTask(Point p) throws RemoteException;
+
+    void addResult(Task t) throws RemoteException;
+
+    Task getTask() throws RemoteException;
+}
